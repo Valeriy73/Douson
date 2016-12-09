580f0167eee3965e1c350cc97de2d1955a1f9c65
Valeriy73 <turlera@ukr.net>
2016-12-09 14:34:12 +0200

    finish chapter 8

--- critter_caretaker4.py
+++ critter_caretaker4.py
@@ -2,21 +2,27 @@
 
 # Моя Зверюшка
 # Виртуальный питомец, о котором пользователь может заботиться
+import random
 
 
 class Critter(object):
     """Виртуальный питомец"""
-    def __init__(self, name, hunger=0, boredom=0):
+
+    total = 0
+
+    def __init__(self, name):
         print "Появилась на свет новая зверюшка!"
         self.name = name
-        self.hunger = hunger
-        self.boredom = boredom
+        self.hunger = random.randint(0, 4)
+        self.boredom = random.randint(0, 4)
+        Critter.total += 1
 
     def __str__(self):
         info = "Объект класса Critter:\n"
         info += "Имя " + self.name + "\n"
         info += "Голод " + str(self.hunger) + "\n"
         info += "Настроение " + str(self.boredom) + "\n"
+        info += "Всего зверушек" + str(Critter.total) + "\n"
         return info
 
     def __pass_time(self):
@@ -40,47 +46,67 @@ class Critter(object):
         print "Меня зовут {0} и сейчас я чувствую себя {1}\n".format(self.name, self.mood)
         self.__pass_time()
 
-    def __eat_col(self):
-        col_crecer = 0
-        while not (0 < col_crecer < 5):
-            col_crecer = int (raw_input("Сколько печенюшек вы хотите дать (от 1 до 4)?:"))
-            if col_crecer < 1:
-                print "Ты жадина!!!"
-            elif col_crecer >4:
-                print "Я столько не съем!!!"
-        return col_crecer
+    
 
-    def eat(self):
-        food = self.__eat_col()
-        print "Мрр... Спасибо!"
+    def eat(self, food):
         self.hunger -= food
         if self.hunger < 0:
             self.hunger = 0
         self.__pass_time
 
-    def play_time(self):
-        time = 0
-        while not (0 < time < 5):
-            time = int (raw_input("Сколько минут ты со мной поиграешь (от 1 до 4)?: "))
-            if time < 1:
-                print "Ты не хочешь со мной играть?"
-            elif time > 4:
-                print "{name} устал, я не могу так долго играть".format (name = self.name)
-        return time
+    
 
-    def play(self):
-        fun = self.play_time()
-        print "Уиии!"
+    def play(self, fun):
+        
+        
         self.boredom -= fun
         if self.boredom < 0:
             self.boredom = 0
         self.__pass_time
 
+def new_animals(number_animal, crit):
+    crit_name = raw_input("Как вы назовете свою зверушку? ")
+    crit.append(Critter(crit_name))
+    number_animal += 1
+    return number_animal, crit
+
+def eat_col():
+        col_crecer = 0
+        while not (0 < col_crecer < 5):
+            col_crecer = int (raw_input("Сколько печенюшек вы нам дадите (от 1 до 4)?:"))
+            if col_crecer < 1:
+                print "Ты жадина!!!"
+            elif col_crecer >4:
+                print "Мы столько не съедим!!!"
+        print "Мрр... Спасибо!"
+        return col_crecer
+
+def play_time():
+        time = 0
+        while not (0 < time < 5):
+            time = int (raw_input("Сколько минут ты будешь с нами грать (от 1 до 4)?: "))
+            if time < 1:
+                print "Ты не хочешь со мной играть?"
+            elif time > 4:
+                print "Мы устали и не можем так долго играть"
+        print "Уиии!"
+        return time
 
 def main():
-    crit_name = raw_input("Как вы назовете свою зверушку? ")
-    crit = Critter(crit_name)
+    crit = []
+    new_animal = "y"
+    number_animal = 0
+    number_animal, crit = new_animals(number_animal, crit)
+    while  new_animal == "y":
+        add_animal = raw_input("Хотите создать еще одну зверушку (y/n)?")
+        if add_animal == "y":
+            number_animal, crit = new_animals(number_animal, crit)
+        elif add_animal == "n":
+            break
+        else:
+             add_animal = raw_input("Выберите y или n.")
 
+    
     choice = None
     while choice != "0":
         print \
@@ -99,16 +125,22 @@ def main():
             print "До свидания."
         # беседа со зверюшкой
         elif choice == "1":
-            crit.talk()
+            for zver in range(0, len(crit)):
+                crit[zver].talk()
         # Кормление зверюшки
         elif choice == "2":
-            crit.eat()
+            food = eat_col()
+            for zver in range(0, len(crit)):
+                crit[zver].eat(food)
         # игра со зверюшкой
         elif choice == "3":
-            crit.play()
+            fun = play_time()
+            for zver in range(0, len(crit)):
+                crit[zver].play(fun)
         # Секретная информация
         elif choice == "4":
-            print crit
+            for zver in range(0, len(crit)):
+                print crit[zver]
         # непонятный пользовательский ввод
         else:
             print "Извините, в меню нет пункта", choice
